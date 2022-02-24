3f1e5bfbe0d8cdc6df644af8efc8fd37d98cf8cd
sleepyheaduwu <99535030+sleepyheaduwu@users.noreply.github.com>
2022-02-24 09:21:06 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -3,18 +3,36 @@
 <title>HOMEPAGE</title>
 </head>
 
-<body background="backgroundu.jpg">
-<font color="white">
+<body background="backgroundu.JPG">
+<font color=white size=6>
 <h1><center>PERFORMANCE TASKS</h1>
 </font>
 <hr>
-<center>
-<table border=5 bgcolor=white width=50%>
-<th>INTRODUCTION</th>
-<th>CAE</th>
-<th>CLE</th>
-<th>MATH</th>
+<p align=”left”>
+<font color=white size=8>
+<tr>
+<br/>
+<td>CAE</td>
+</tr>
+<br>
+<br>
+<br/>
+<tr>
+<td>CLE</td>
+</tr>
+<br>
+<br>
+<br/>
+<tr>
+<td>MATH</td>
+</tr>
+<br>
+<br>
+<hr>
+<font color=white size=3>
+<ul>
+<h1>Group 4</h1>
 </table>
-</center>
+</left>
 </body>
 </html>
